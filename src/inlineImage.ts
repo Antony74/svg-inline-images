c62bd841cf1bf508b74b6d56b16814460dff54a3
Antony74 <akb@akb.me.uk>
2025-05-24 19:43:28 +0100

    refactor: inlineImage into two functions

--- src/inlineImage.ts
+++ src/inlineImage.ts
@@ -6,38 +6,46 @@ export const inlineImage = async (
     image: SVGImageElement,
     fetchLite: FetchLite
 ): Promise<void> => {
-    for (const attributeName of ['xlink:href', 'href']) {
-        let urlOrPath = image.getAttribute(attributeName);
+    await inlineImageAttribute(image, fetchLite, 'xlink:href');
+    await inlineImageAttribute(image, fetchLite, 'href');
+};
 
-        if (!urlOrPath) {
-            continue;
-        }
+const inlineImageAttribute = async (
+    image: SVGImageElement,
+    fetchLite: FetchLite,
+    attributeName: string
+): Promise<void> => {
+    let urlOrPath = image.getAttribute(attributeName);
 
-        try {
-            const url = new URL(urlOrPath);
-            switch (url.protocol) {
-                case 'file:':
-                    urlOrPath = urlOrPath.slice(7);
-                    break;
-                case 'data:':
-                    continue; // image is already inline
-            }
-        } catch {
-            // Assume it was a path not a url - whether it's any good or not is a matter for the fetcher
-        }
-
-        const buffer = await fetchLiteFetch(urlOrPath, fetchLite);
-
-        const content = Buffer.from(buffer).toString('base64');
-
-        const mimeType = mime.lookup(urlOrPath);
-
-        if (mimeType === false) {
-            throw new Error(`Failed to find a mime-type for '${urlOrPath}'`);
-        }
-
-        const newUrl = `data:${mimeType};base64, ${content}`;
-
-        image.setAttribute(attributeName, newUrl);
+    if (!urlOrPath) {
+        return;
     }
+
+    try {
+        const url = new URL(urlOrPath);
+        switch (url.protocol) {
+            case 'file:':
+                // lose the prefix 'file://' as neither fetch or fs.promises.readFile support it
+                urlOrPath = urlOrPath.slice(7);
+                break;
+            case 'data:':
+                return; // image is already inline
+        }
+    } catch {
+        // Assume it was a path not a url - whether it's any good or not is a matter for the fetcher
+    }
+
+    const buffer = await fetchLiteFetch(urlOrPath, fetchLite);
+
+    const content = Buffer.from(buffer).toString('base64');
+
+    const mimeType = mime.lookup(urlOrPath);
+
+    if (mimeType === false) {
+        throw new Error(`Failed to find a mime-type for '${urlOrPath}'`);
+    }
+
+    const newUrl = `data:${mimeType};base64, ${content}`;
+
+    image.setAttribute(attributeName, newUrl);
 };
